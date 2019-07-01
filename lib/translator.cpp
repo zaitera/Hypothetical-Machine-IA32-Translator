1ae4de5d5a1a3d82732c916ab5a57f9aa684f6c7
Abdullah <abdullah.zaiter@hotmail.com>
2019-06-30 23:53:59 -0300

    done translation, added support for all instructions and data directives

--- lib/translator.cpp
+++ lib/translator.cpp
@@ -19,22 +19,15 @@ void Translator::solveDisplacement(std::vector<std::string> line, uint16_t curre
     bool used = false;
     if(line.size()-current_token>3 && line[current_token+2] == "+"){
         used = true;
-        this->output_file << "push esi\n";
-        this->output_file << "mov esi, "+ line[current_token+3]+"\n";
-        this->output_file << metadata <<" + " << "esi*4";
+        this->output_file << metadata <<" + " << std::stoi(line[current_token+3])*4;
     };       
     if (!used)
     {
         this->output_file << metadata;
     }
-    if(line[current_token].substr(0, 3) != "JMP")
+    if(line[current_token].substr(0, 3) != "JMP" && line[current_token]!="S_INPUT" && line[current_token]!="S_OUTPUT")
         this->output_file << "]";
     this->output_file << "\n";
-    if (used)
-    {
-        this->output_file << "pop esi\n";
-    }
-    
 }
 
 void Translator::translate(void)
@@ -53,33 +46,43 @@ void Translator::translate(void)
     this->output_file<<"global _start\n";
     uint16_t current_token;
     Section section = UNDEFINED;
+    std::vector<std::string> functs_to_load;
     for(size_t i = 0; i != this->file_being_translated.size(); i++ )
     {
         auto line = std::get<1>(this->file_being_translated[i]);
         current_token = 0;
+        this->output_file << ";;;;";
+        for (size_t i = 0; i < line.size(); i++)
+        {
+            this->output_file <<line[i]<<" ";
+        }
+		this->output_file << ";;;;\n";
 
         if (line[0] == "SECTION")
         {
             if(line[1] == "TEXT")
             {
                 section = TEXT;
-                this->output_file<<".text\n";
+                this->output_file<<"section .text\n";
                 this->output_file<<"__start:\n";                
             }else if(line[1] == "DATA") 
             {
                 section = DATA;
+                this->output_file<<"section .data\n";
             }
             continue;
         }
         if(line.size()>=2 && line[1] == ":")
         {
-            this->output_file<<line[0]+":";
+            this->output_file<<line[0]+": ";
             current_token += 2;
         } 
         if(line.size()==current_token){
             this->output_file<<"\n";
             continue;
         }
+        
+        
         std::string aux;
         switch (section)
         {
@@ -91,128 +94,214 @@ void Translator::translate(void)
                         aux = "   ";
                         std::transform(line[current_token].begin(), line[current_token].end(),aux.begin(), ::tolower);
                         solveDisplacement(line,current_token, aux+" eax, dword["+line[current_token+1]);
-                        // this->output_file << aux << " eax, dword[" << line[current_token+1];
                         break;
                     case HashStringToInt("MULT"):
                         solveDisplacement(line,current_token,"imul dword[");
-                        // this->output_file << "imul dword[" << line[current_token+1];
                         break;
                     case HashStringToInt("DIV"):
                         this->output_file << "cdq\n";
                         solveDisplacement(line,current_token,"idiv dword["+line[current_token+1]);
-                        // this->output_file << "idiv dword[" << line[current_token+1];
                         break;
                     case HashStringToInt("JMP"):
                         solveDisplacement(line,current_token,"jmp "+line[current_token+1]);
-                        // this->output_file << "jmp " <<line[current_token+1];
                         break;
                     case HashStringToInt("JMPP"):
                         this->output_file << "cmp eax, 0" << std::endl;
                         solveDisplacement(line,current_token,"jg " + line[current_token+1]);
-		                // this->output_file << "jg " << line[current_token+1];
                         break;
                     case HashStringToInt("JMPN"):
                         this->output_file << "cmp eax, 0" << std::endl;
                         solveDisplacement(line,current_token,"jl "+ line[current_token+1]);
-		                // this->output_file << "jl " << line[current_token+1];
                         break;
                     case HashStringToInt("JMPZ"):
                         this->output_file << "cmp eax, 0" << std::endl;
                         solveDisplacement(line,current_token,"je "+line[current_token+1]);
-		                // this->output_file << "je " << line[current_token+1];
                         break;
+                    case HashStringToInt("COPY"):
+                        solveDisplacement(line,current_token,"mov ebx, dword["+line[current_token+1]);
+                        if (line.size()-current_token>5 && line[line.size()-2] == "+")
+                        {
+                            this->output_file << "mov dword["<< line[line.size()-3]<<" + " << std::stoi(line[line.size()-1])*4<<"], ebx\n";                        
+                        }else
+                        {
+                            this->output_file << "mov dword["<< line[line.size()-1]<<"]"<<", ebx\n";
+                        }
+                        break;
+                    case HashStringToInt("LOAD"):
+                        solveDisplacement(line,current_token,"mov eax, dword["+line[current_token+1]);
+                        break;
+                    case HashStringToInt("STORE"): 
+                        if(line.size()-current_token>3 && line[current_token+2] == "+"){
+                            this->output_file << "mov dword["<<line[current_token+1] <<" + " << std::stoi(line[current_token+3])*4<<"], eax\n";
+                        }else
+                        {
+                            this->output_file << "mov dword["<<line[current_token+1] <<"], eax\n";
+                            
+                        }
+                        break;
+                    case HashStringToInt("INPUT"):
+                        this->output_file << "push ebx\n";
+                        this->output_file << "push ecx\n";
+                        this->output_file << "push edx\n";
+                        
+                        solveDisplacement(line,current_token,"push dword["+line[current_token+1]);
+                        
+                        this->output_file << "call LerInteiro\n";
+
+                        this->output_file << "pop edx\n";
+                        this->output_file << "pop ecx\n";
+                        this->output_file << "pop ebx\n";
+                        functs_to_load.push_back("readInt");
+                        break;
+                    case HashStringToInt("OUTPUT"):
+                        this->output_file << "push ebx\n";
+                        this->output_file << "push ecx\n";
+                        this->output_file << "push edx\n";
+                        
+                        solveDisplacement(line,current_token,"push dword["+line[current_token+1]);
+                        
+                        this->output_file << "call EscreverInteiro\n";
+
+                        this->output_file << "pop edx\n";
+                        this->output_file << "pop ecx\n";
+                        this->output_file << "pop ebx\n";
+                        functs_to_load.push_back("writeInt");
+                        break;
+                    case HashStringToInt("C_INPUT"):
+                        this->output_file << "push ebx\n";
+                        this->output_file << "push ecx\n";
+                        this->output_file << "push edx\n";
+                        
+                        solveDisplacement(line,current_token,"push dword["+line[current_token+1]);
+                        
+                        this->output_file << "call LerChar\n";
+
+                        this->output_file << "pop edx\n";
+                        this->output_file << "pop ecx\n";
+                        this->output_file << "pop ebx\n";
+                        functs_to_load.push_back("readChar");
+                        break;
+                    case HashStringToInt("C_OUTPUT"):
+                        this->output_file << "push ebx\n";
+                        this->output_file << "push ecx\n";
+                        this->output_file << "push edx\n";
+                        
+                        solveDisplacement(line,current_token,"push dword["+line[current_token+1]);
+                        
+                        this->output_file << "call EscreverChar\n";
+
+                        this->output_file << "pop edx\n";
+                        this->output_file << "pop ecx\n";
+                        this->output_file << "pop ebx\n";
+                        functs_to_load.push_back("writeChar");
+                        break;
+                    case HashStringToInt("S_INPUT"):
+                        this->output_file << "push ebx\n";
+                        this->output_file << "push ecx\n";
+                        this->output_file << "push edx\n";
+                        
+                        this->output_file << "push " << line[line.size()-1] << "\n";
                     
+                        solveDisplacement(line,current_token,"mov esi, "+line[current_token+1]);
+                        this->output_file << "push esi\n";
+
+                        this->output_file << "call LerString\n";	
+
+                        this->output_file << "pop edx\n";
+                        this->output_file << "pop ecx\n";
+                        this->output_file << "pop ebx\n";
+                        functs_to_load.push_back("readString");
+                        break;
+                    case HashStringToInt("S_OUTPUT"):
+                        this->output_file << "push ebx\n";
+                        this->output_file << "push ecx\n";
+                        this->output_file << "push edx\n";
+
+                        this->output_file << "push " << line[line.size()-1] << "\n";
+                        
+
+                        solveDisplacement(line,current_token,"mov esi, "+line[current_token+1]);
+                        this->output_file << "push esi\n";
+
+                        
+                        this->output_file << "call EscreverString\n";	
+
+                        this->output_file << "pop edx\n";
+                        this->output_file << "pop ecx\n";
+                        this->output_file << "pop ebx\n";
+                        functs_to_load.push_back("writeString");
+                        break;
+                    case HashStringToInt("STOP"):{
+                        this->output_file << "__exit:\n";
+                        this->output_file << "mov eax, 1\n";
+                        this->output_file <<"mov ebx, 0\n";
+                        this->output_file <<"int 0x80\n";
+                        this->output_file <<"\n";
+                        this->output_file <<";USED IO FUNCTIONS:\n";
+                        this->output_file <<"\n";
+                        
+                        //delete duplicates
+                        sort( functs_to_load.begin(), functs_to_load.end() );
+                        functs_to_load.erase( unique( functs_to_load.begin(), functs_to_load.end() ), functs_to_load.end());
+                        
+                        std::string text_line;
+                        std::fstream funct_code;
+                        for (size_t i = 0; i < functs_to_load.size(); i++)
+                        {
+                            funct_code.open("./functs/"+functs_to_load[i]+".s",std::ios::in);
+                            while (std::getline(funct_code, text_line))
+                            {
+                                this->output_file<<text_line<<"\n";
+                                
+                            }
+                            this->output_file<<"\n";
+                            funct_code.close();
+                        }
+                    }
                     default:
                         break;
                 }                
                 break;
             case DATA:
-
+                if (line[current_token] == "CONST")
+                {
+                    long pos = this->output_file.tellp(); // gets the current position of the buffer 
+                    this->output_file.seekp(pos - 2); //to remove the : char which is not used in ia32
+                    this->output_file <<" dd " << line[current_token+1] << "\n";
+                }else if (line[current_token] == "SPACE")
+                {
+                    size_t spaces = 0;
+                    if (line.size()-current_token>1)
+                    {
+                        spaces = std::stoi(line[current_token+1]);
+                    }else
+                    {
+                        spaces = 1;
+                    }
+                    long pos = this->output_file.tellp(); // gets the current position of the buffer 
+                    this->output_file.seekp(pos - 2); //to remove the : char which is not used in ia32
+                    this->output_file <<" dd ";
+                    
+                    do{
+                        this->output_file <<"00,";
+                        spaces--;
+                    }while(spaces > 0);
+                    pos = this->output_file.tellp(); // gets the current position of the buffer 
+                    this->output_file.seekp(pos - 1); //to remove the last ,  added in the loop
+                    this->output_file <<"\n";      
+                }
+                
+                
                 break;
             
             default:
                 break;
         }
+        
 
-		// else if(this->master.substr(0, 3) == "JMP"){
-		// 	// Deal with jumps
-		// 	if(this->master.length() == 3){
-		// 		// Unconditional jump
-		// 		file << "jmp " << this->args[0] << endl;				
-		// 	}
-		// 	else {
-		// 		// Compare eax with 0
-		// 		file << "cmp eax, 0" << endl;
-				
-		// 		switch(this->master[3]){
-		// 			case 'P':
-		// 				file << "jg " << this->args[0] << endl;
-		// 				break;
-		// 			case 'N':
-		// 				file << "jl " << this->args[0] << endl;
-		// 				break;
-		// 			case 'Z':
-		// 				file << "je " << this->args[0] << endl;
-		// 				break;
-		// 			default:
-		// 				break;			
-		// 		};
-		// 	};
-		// }
-		// else if(this->master == "COPY"){
-		// 	// Copies first element into eax
-		// 	file << "mov eax, dword[" << this->args[0];
-			
-		// 	// Check if a '+' was used in the invented assembler
-		// 	if(this->args[1] == "+"){
-		// 		file << " + " << this->args[2];
-		// 	};
-			
-		// 	file << "]" << endl;
-			
-		// 	// Copies eax into second element
-		// 	if(this->args[1] == "+"){
-		// 		// Use 3rd argument
-		// 		file << "mov dword[" << this->args[3];
-			
-		// 		// Check if a '+' was used in the invented assembler
-		// 		if(this->args[4] == "+"){
-		// 			file << " + " << this->args[5];
-		// 		};
-			
-		// 		file << "], eax" << endl;				
-		// 	} 
-		// 	else {
-		// 		file << "mov dword[" << this->args[1];
-			
-		// 		// Check if a '+' was used in the invented assembler
-		// 		if(this->args[2] == "+"){
-		// 			file << " + " << this->args[3];
-		// 		};
-			
-		// 		file << "], eax" << endl;	
-		// 	};
-		// }
-		// else if(this->master == "LOAD"){
-		// 	file << "mov eax, dword[" << this->args[0];
-			
-		// 	// Check if a '+' was used in the invented assembler
-		// 	if(this->args[1] == "+"){
-		// 		file << " + " << this->args[2];
-		// 	};
-			
-		// 	file << "]" << endl; 
-		// }
-		// else if(this->master == "STORE"){
-		// 	file << "mov dword[" << this->args[0];
-		
-		// 	// Check if a '+' was used in the invented assembler
-		// 	if(this->args[1] == "+"){
-		// 		file << " + " << this->args[2];
-		// 	};
-		
-		// 	file << "], eax" << endl;	
-		// }
+	
+        // STOP
+        // 
     }
 
     this->output_file.close();
